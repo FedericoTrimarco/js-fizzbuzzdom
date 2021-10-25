2f7c808b0215e28efc404c1ab00d87bb5aced100
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-25 17:34:59 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -7,27 +7,31 @@ const boxes = document.querySelector('.boxes');
 
 // ciclo da 1 a 100
 for (let i = 1; i <= 100; i++){
-    
+
+    let className;
+    let text;
+
     //  condizione multipli di 3 e 5
     if(i % 3 == 0 && i % 5 == 0){
-        console.log(i, 'multiplo di 3 e 5');
-        boxes.innerHTML += `<div class="box box-red">FizzBuzz</div>`;
+        className = 'box-red';
+        text = 'FizzBuzz';
     }
 
     // condizione multipli di 5
     else if(i % 5 == 0){
-        console.log(i, 'multiplo di 5');
-        boxes.innerHTML += `<div class="box box-yellow">Buzz</div>`;
+        className = 'box-yellow';
+        text = 'Buzz';
     }
 
     // condizione multipli di 3
     else if(i % 3 == 0){
-        console.log(i, 'multiplo di 3');
-        boxes.innerHTML += `<div class="box box-green">Fizz</div>`;
+        className = 'box-green';
+        text = 'Fizz';
     }
 
     else{
-        console.log(i);
-         boxes.innerHTML += `<div class="box box-general">${i}</div>`;
+        className = 'box-general';
+        text = i;
     }
+    boxes.innerHTML += `<div class="box ${className}">${text}</div>`;
 }
